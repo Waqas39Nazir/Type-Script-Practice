6a0fe7192517c3247209be19d542c357681302ce
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-06 22:04:40 +0500

    String Typing

--- step-05-strong-typing/index.js
+++ step-05-strong-typing/index.js
@@ -1,4 +1,7 @@
 "use strict";
+// No type annotation needed -- 'dummyName' inferred as type 'string'
+const dummyName = "Waqas";
+// Type Annotations on Variables
 const personName = "Waqas Nazir";
 console.log("Person Name:", personName);
 const personOne = {
@@ -22,3 +25,32 @@ console.log("isDeveloper:", isDeveloper);
 const nameArray = ["Waqas", "Waqar", "Hassan", "Ahsan"];
 // Note: number[] (Used for Arrays) and [number](Used for Tuples) are a different things
 console.log("Name Array:", nameArray);
+//Functions
+const addNumbers = (a, b) => {
+    return a + b;
+};
+const concactStrings = (str1, str2) => {
+    //   return str1 + " " + str2;
+    return str1.concat(` ${str2}`);
+};
+console.log("Add Numbers:", addNumbers(4, 10));
+console.log("Concact Strings:", concactStrings("Waqas", "Nazir"));
+//Functions that return a Promise
+const myFunction = (num) => {
+    return new Promise((resolve, reject) => {
+        if (typeof num === "number") {
+            resolve(num);
+            //   reject("Error");
+        }
+        // else {
+        //   reject("Error");
+        // }
+    });
+};
+myFunction(9)
+    .then((result) => {
+    console.log(result);
+})
+    .catch((error) => {
+    console.log(error);
+});
--- step-05-strong-typing/index.ts
+++ step-05-strong-typing/index.ts
@@ -45,3 +45,38 @@ const nameArray: string[] = ["Waqas", "Waqar", "Hassan", "Ahsan"];
 // Note: number[] (Used for Arrays) and [number](Used for Tuples) are a different things
 
 console.log("Name Array:", nameArray);
+
+//Functions
+const addNumbers = (a: number, b: number): number => {
+  return a + b;
+};
+
+const concactStrings = (str1: string, str2: string): string => {
+  //   return str1 + " " + str2;
+  return str1.concat(` ${str2}`);
+};
+
+console.log("Add Numbers:", addNumbers(4, 10));
+
+console.log("Concact Strings:", concactStrings("Waqas", "Nazir"));
+
+//Functions that return a Promise
+const myFunction = (num: number): Promise<number> => {
+  return new Promise((resolve, reject) => {
+    if (typeof num === "number") {
+      resolve(num);
+      //   reject("Error");
+    }
+    // else {
+    //   reject("Error");
+    // }
+  });
+};
+
+myFunction(9)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });

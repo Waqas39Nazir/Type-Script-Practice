4e89092d8addae0ab34e32c4093b1130a99544b1
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-06 21:09:49 +0500

    Types Error

---
+++ step-04-assignment-error/README.md
@@ -0,0 +1,7 @@
+1. If we write
+   let message = "Waqas Nazir";
+   then want to assign another value other than string
+   message = 26 // we will get error Type 'number' is not assignable to type 'string'.
+
+2. although we have type errors in our code but the index.js file will be created after running
+   tsc
---
+++ step-04-assignment-error/index.ts
@@ -0,0 +1,11 @@
+let message = "Waqas Nazir";
+
+message = 6; // Type 'number' is not assignable to type 'string'.
+
+//The better aprroach to write the above code is
+let message2: string = "Waqas Nazir";
+
+message2 = { name: "Waqas" }; //Type '{ name: string; }' is not assignable to type 'string'.
+
+console.log("Message:", message);
+console.log("Message 2:", message2);

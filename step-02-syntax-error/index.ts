a9662e33852cc4097c2343e95e4a3075feb8e8be
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-06 20:53:21 +0500

    Types Error

--- step-02-syntax-error/index.ts
+++ step-02-syntax-error/index.ts
@@ -1,1 +1,1 @@
-lett message = "Hello World";//syntax error
+// lett message = "Hello World";//syntax error
---
+++ step-03-type-error/index.ts
@@ -0,0 +1,2 @@
+let message = "Hello World";
+// console.loger(message); // These erros occurs becuase of typing mistakes

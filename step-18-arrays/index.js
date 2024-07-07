df399ee75a372ca16d53cb22e84e0163fbbe3a50
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-07 14:13:13 +0500

    Arrays

---
+++ step-18-arrays/index.js
@@ -0,0 +1,21 @@
+"use strict";
+// arrays of strings
+const arrayOfStrings = [
+    "Waqas Nazir",
+    "Waqar Nazir",
+    "Hassan Nazir",
+    "Ahsan Nazir",
+];
+console.log("Array of Strings:", arrayOfStrings);
+//array of numbers
+const arrayOfNumbers = [1, 3, 6, 3, 4, 63, 2];
+console.log("Array of Numbers:", arrayOfNumbers);
+//arrays of strings and numbers
+const arrayOfStringsAndNumbers = [1, 2, 3, "String"];
+console.log("Array of Strings and Numbers:", arrayOfStringsAndNumbers);
+// const arrayOfObjects: IPerson[] = []; success
+const arrayOfObjects = [
+    { name: "Waqas Nazir", age: "26" },
+    { name: "Muhammad Waqar Nazir", age: "23" },
+];
+console.log("Array Of Objects:", arrayOfObjects);
---
+++ step-18-arrays/index.ts
@@ -0,0 +1,30 @@
+// arrays of strings
+const arrayOfStrings: string[] = [
+  "Waqas Nazir",
+  "Waqar Nazir",
+  "Hassan Nazir",
+  "Ahsan Nazir",
+];
+console.log("Array of Strings:", arrayOfStrings);
+
+//array of numbers
+const arrayOfNumbers: number[] = [1, 3, 6, 3, 4, 63, 2];
+console.log("Array of Numbers:", arrayOfNumbers);
+
+//arrays of strings and numbers
+const arrayOfStringsAndNumbers: (number | string)[] = [1, 2, 3, "String"];
+console.log("Array of Strings and Numbers:", arrayOfStringsAndNumbers);
+
+//array of objects
+interface IPerson {
+  name: string;
+  age: string;
+}
+
+// const arrayOfObjects: IPerson[] = []; success
+const arrayOfObjects: IPerson[] = [
+  { name: "Waqas Nazir", age: "26" },
+  { name: "Muhammad Waqar Nazir", age: "23" },
+];
+
+console.log("Array Of Objects:", arrayOfObjects);

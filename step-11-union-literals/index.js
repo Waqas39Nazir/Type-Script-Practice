5522aafca29d7ca2b1f5d838d1d747601e792ed4
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-07 10:38:17 +0500

    Union Literals Type Narrowing Arrays Methods

---
+++ step-11-union-literals/index.js
@@ -0,0 +1,30 @@
+"use strict";
+const printHandler = (a) => {
+    // return a;
+    if (typeof a === "string") {
+        a = a.toUpperCase();
+        //   This will not convert the string to lower case
+        // the reason is that in JavaScript strings are immutable
+        //this expression below will return a new string and you have to assign that to a
+        //   a.toLowerCase();
+    }
+    console.log("Number | String:", a);
+};
+printHandler(5);
+printHandler("Waqas");
+const commandMethods = (x) => {
+    //   return x.slice(0, 3);
+    return x.slice(3, 15);
+};
+const dummyArray = [3, 6, 9, 3, 12, 15];
+const dummyString = "Waqas Nazir";
+console.log(commandMethods(dummyArray));
+console.log(dummyArray);
+console.log(commandMethods(dummyString));
+console.log(dummyArray);
+const spliceHandler = (x) => {
+    return x.splice(1, 3, 4, 5, 6, 7, 8, 9, 10);
+};
+//splice only works on arrays
+console.log("Splice Method:", spliceHandler(dummyArray));
+console.log(dummyArray);

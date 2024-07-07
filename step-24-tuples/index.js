34f30a6f19fcea4067beeef24f7fa251995b5f78
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-07 17:19:07 +0500

    Tuples

---
+++ step-24-tuples/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let tuples;
+tuples = [1, 3, "Waqas", false];
+console.log("Tuples:", tuples);
---
+++ step-24-tuples/index.ts
@@ -0,0 +1,9 @@
+let tuples: [number, number, string, boolean];
+
+tuples = [1, 3, "Waqas", false];
+
+console.log("Tuples:", tuples);
+
+type testTuple = [number, string, number];
+
+const test: testTuple = [1, "Waqas", 1];

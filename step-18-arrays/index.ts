8d02a7eaea518f00e871bec8effb1c050bba489a
Waqas Nazir <waqasnazir@altaurux.com>
2024-07-07 14:14:27 +0500

    Arrays

--- step-18-arrays/index.ts
+++ step-18-arrays/index.ts
@@ -10,6 +10,7 @@ console.log("Array of Strings:", arrayOfStrings);
 //array of numbers
 const arrayOfNumbers: number[] = [1, 3, 6, 3, 4, 63, 2];
 console.log("Array of Numbers:", arrayOfNumbers);
+// let array2:Array<number> = [1, 2, 3];//alternative correct syntax
 
 //arrays of strings and numbers
 const arrayOfStringsAndNumbers: (number | string)[] = [1, 2, 3, "String"];
